6281d1afc1f46a5dc74bfc38244809e09ccc4a3e
p <p@DESKTOP-RRG3R1N>
2019-08-03 15:07:05 +0900

    구매 완료!!

--- src/com/kh/bvengers/product/model/dao/ProductDao.java
+++ src/com/kh/bvengers/product/model/dao/ProductDao.java
@@ -979,6 +979,27 @@ public class ProductDao {
 		return productPay;
 	}
 
+	public int deleteBacket(Connection con, String codeList) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("deleteBacket");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setString(1, codeList);
+			
+			result = pstmt.executeUpdate();
+			
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			close(pstmt);
+		}
+		return result;
+	}
+
 	
 }
 
